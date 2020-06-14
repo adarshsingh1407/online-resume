0cbc11949ee78fb90699b52e1f9fe957f8267f82
Adarsh Singh <adasingh2@publicisgroupe.net>
2020-06-14 13:35:55 +0530

    Setup eslint and prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 function App() {
   return (
@@ -8,7 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <span>Edit</span>
+          <code>src/App.js</code>
+          <span>and save to reload.</span>
         </p>
         <a
           className="App-link"
@@ -20,7 +22,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
