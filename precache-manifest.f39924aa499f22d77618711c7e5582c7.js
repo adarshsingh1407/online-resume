9909346a505242ebe3d696a61af822ab0f924b80
Adarsh Singh <adarshsingh1407@gmail.com>
2020-06-14 14:00:42 +0530

    Updates

---
+++ precache-manifest.f39924aa499f22d77618711c7e5582c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36a0160ef4316972dde780a91989acbc",
+    "url": "/online-resume/index.html"
+  },
+  {
+    "revision": "8de42c0e5aa8f4c20279",
+    "url": "/online-resume/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9361150a42235ed433fd",
+    "url": "/online-resume/static/js/2.fdbc0b26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/online-resume/static/js/2.fdbc0b26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8de42c0e5aa8f4c20279",
+    "url": "/online-resume/static/js/main.e56f3a46.chunk.js"
+  },
+  {
+    "revision": "a6c2ac5f64e7f8fdca47",
+    "url": "/online-resume/static/js/runtime-main.a69f1745.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/online-resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.00604474c08115667ec9147f27a98929.js"
+  "/online-resume/precache-manifest.f39924aa499f22d77618711c7e5582c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/online-resume/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
