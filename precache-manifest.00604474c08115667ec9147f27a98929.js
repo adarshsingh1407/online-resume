eaa9b3d94ef5df6e7443879f71577e62150fd395
Adarsh Singh <adasingh2@publicisgroupe.net>
2020-06-14 13:44:40 +0530

    Updates

---
+++ precache-manifest.00604474c08115667ec9147f27a98929.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165f881308c2c9670b801d65c5f4d265",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e78a6dedba76aa2b879",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9361150a42235ed433fd",
+    "url": "/static/js/2.fdbc0b26.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fdbc0b26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e78a6dedba76aa2b879",
+    "url": "/static/js/main.398e9676.chunk.js"
+  },
+  {
+    "revision": "3b7cdb764df6e685ac0e",
+    "url": "/static/js/runtime-main.2a6c6e27.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
